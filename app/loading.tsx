afe576bfdea00670872965ec4bdc56f9efc388b7
Hariom Vishwkarma <62866892+mlhkhariom@users.noreply.github.com>
2025-02-21 09:45:44 +0530

    unzipgit

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import { Spinner } from "@/components/ui/spinner"
+
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Spinner size="lg" />
+    </div>
+  )
+}
+
